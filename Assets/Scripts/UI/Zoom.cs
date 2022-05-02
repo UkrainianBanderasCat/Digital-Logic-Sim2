589bfae21a6735150a2e28ce9ce6036d92c5d56a
Haccer <andreyev.ura@gmail.com>
2022-05-02 22:49:38 +0300

    Zoom and led

--- Assets/Scripts/UI/Zoom.cs
+++ Assets/Scripts/UI/Zoom.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Zoom : MonoBehaviour
 {
     public Vector3 scaleChange;
-    
+    Vector3 scale = new Vector3(1, 1, 1);
+    public float min;
+    public float max;
+    float interactionNum = 0.25f;
+    public GameObject[] objectsToZoom;
+    public ChipInteraction _chipInteraction;
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown("=")) {
-            //ZoomIn
+        _chipInteraction = GameObject.FindWithTag("Interaction").GetComponent<ChipInteraction>(); 
+        objectsToZoom = GameObject.FindGameObjectsWithTag("Zoom");
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown("=") && scale.x < max) {
+            scale += scaleChange;
+            interactionNum += 0.1f;
         }
 
-        else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown("-")) {
-            //ZoomOut
-        } 
+        else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown("-") && scale.x > min) {
+            scale -= scaleChange;
+            interactionNum -= 0.1f;
+        }
+
+        _chipInteraction.selectionBoundsBorderPadding = interactionNum;
+        for (int i = 0; i < objectsToZoom.Length; i++) {
+            objectsToZoom[i].transform.localScale = scale;
+        }
     }
 }
