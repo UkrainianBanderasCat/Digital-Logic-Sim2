3d4a6c6f45d9837a3cbf31634e7acb283d83c74f
firecerne <105079722+firecerne@users.noreply.github.com>
2022-06-18 00:47:20 +0200

    Improved Chip Delete
    I improved ChipDelete.cs to be able to choose between deleting only the local save files or every save files (local and global) .

--- Assets/Scripts/Save System/ChipDelete.cs
+++ Assets/Scripts/Save System/ChipDelete.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class ChipDelete : MonoBehaviour
 {
     new string name;
-	const string fileExtension = ".txt";
+    const string fileExtension = ".txt";
     Manager _manager;
     public GameObject deleteConfirmation;
     public TextMeshProUGUI confirmText;
@@ -16,26 +16,37 @@ public class ChipDelete : MonoBehaviour
         _manager = GameObject.FindWithTag("Manager").GetComponent<Manager>();
     }
 
-    public void Delete(string _name) {
+    public void ConfirmDelete(string _name) {
         deleteConfirmation.SetActive(true);
         confirmText.text = "Do you want to delete chip:" + "\n" + _name + "\n" + "If it used in other chips," + "\n" + "it can break this project";
         name = _name;
     }
 
-    public void ConfirmDelete()
+    public void DeleteLocal()
     {
         string deletePath = SaveSystem.GetPathToSaveFile(name);
+        string wireDeletePath = SaveSystem.GetPathToWireSaveFile(name);
+        DeleteFile(deletePath);
+        DeleteFile(wireDeletePath);
+    }
+   
+    public void DeleteGlobal()
+    {
         string deleteGlobalPath = SaveSystem.GetPathToGlobalSaveFile(name);
+        string wireDeleteGlobalPath = SaveSystem.GetPathToGlobalWireSaveFile(name);
 
-        string wireDeletePath = SaveSystem.GetPathToSaveFile(name);
-        string wireDeleteGlobalPath = SaveSystem.GetPathToGlobalSaveFile(name);
-        Debug.Log(deletePath);
-        if (File.Exists(deletePath)) {
-            File.Delete(deletePath);
-            File.Delete(deleteGlobalPath);
+        DeleteFile(deleteGlobalPath);
+        DeleteFile(wireDeleteGlobalPath);
+
+        DeleteLocal();
+    }
+
+    void DeleteFile(string path)
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
             _manager.RefreshAll();
         }
-
-        else { Debug.Log("File doesn't exist"); }
     }
 }
