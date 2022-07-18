cc88d4eea7c82f70e58deb930391d5e4215eaaf5
firecerne <105079722+firecerne@users.noreply.github.com>
2022-07-18 12:23:36 +0200

    Revert "Revert "Merge branch 'master' into master""
    This reverts commit 8dc3f96f8cef964d0c3ee8e5f7bea00dee63a809.

--- Assets/Scenes/TestScript.cs
+++ Assets/Scenes/TestScript.cs
@@ -1,27 +1,74 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using System.IO;
 using System;
 
 public class TestScript : MonoBehaviour
 {
-    public List<string> chars = new List<string>();
-    // Start is called before the first frame update
-    void Start()
+
+    [Serializable]
+    public class Translation
     {
-        
+        public string id;
+        public string text;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.inputString!="")
-        {
-            chars = new List<string>();
-            char tmp=Input.inputString[0];
-            int temp=(int)tmp;
+    public List<Translation> translations = new List<Translation>();
 
-            Debug.Log(temp.ToString());
+    public string selectedLanguage = "English";
+    public List<String> avaibleLanguages = new List<String>();
+    string directoryPath = "";
+    public FileInfo[] info;
+    
+    void Awake()
+    {
+        directoryPath = Application.dataPath + "/StreamingAssets/Localization/";
+        DirectoryInfo dir = new DirectoryInfo(directoryPath);
+        info = dir.GetFiles("*.txt");
+
+        Debug.Log(info);
+        Debug.Log(info.Length);
+        
+        foreach (FileInfo f in info) 
+        {   
+            string filePath = f.ToString();
+            Debug.Log(filePath);
+
+            string fileNameWithTxt = filePath.Replace(directoryPath, "");
+            avaibleLanguages.Add(fileNameWithTxt.Substring(0, fileNameWithTxt.Length - 4));
+
+            StreamReader reader = new StreamReader(filePath); 
+            Debug.Log(reader.ReadToEnd());
+            reader.Close();
+        } 
+
+        //int lines = File.ReadAllLines(directoryPath + selectedLanguage + ".txt").Length;
+        string[] lines = System.IO.File.ReadAllLines(directoryPath + selectedLanguage + ".txt");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            Translation translation = new Translation();
+            
+            Debug.Log(lines[i]);
+            string[] splitArray = lines[i].Split(char.Parse("="));
+
+            translation.id = splitArray[0];
+            translation.text = splitArray[1];
+
+            translations.Add(translation); 
+        }         
+    }
+
+    public string GetText(string id)
+    {
+        for (int i = 0; i < translations.Count; i++)
+        {
+            if (translations[i].id == id)
+            {
+                return translations[i].text;
+            }
         }
+
+        return "";
     }
 }
