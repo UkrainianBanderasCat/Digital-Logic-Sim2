47447190a4aa7fd1332fa960f39b430687f476ea
Haccer <andreyev.ura@gmail.com>
2022-08-15 11:54:08 +0300

    Fixed localization macOS

--- Assets/Scripts/Translation/Localiation.cs
+++ Assets/Scripts/Translation/Localiation.cs
@@ -29,10 +29,10 @@ public class Localiation : MonoBehaviour
         if (PlayerPrefs.GetString("selectedLanguage") != "")
             selectedLanguage = PlayerPrefs.GetString("selectedLanguage");
 
-        #if UNITY_STANDALONE_WIN & UNITY_STANDALONE_LINUX & UNITY_EDITOR
+        #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_EDITOR
             directoryPath = Application.dataPath + "/StreamingAssets/Localization/";
-        #else if UNITY_STANDALONE_OSX
-            directoryPath = Application.dataPath + "/Resources/Data/StreamingAssets/Localization/"
+        #elif UNITY_STANDALONE_OSX
+            directoryPath = Application.dataPath + "/Resources/Data/StreamingAssets/Localization/";
         #endif
 
         DirectoryInfo dir = new DirectoryInfo(directoryPath);
