01888e5b78ed1b3d18e8c6c8f6881a02840f7c6b
TheRiggedDev <109687890+TheRiggedDev@users.noreply.github.com>
2022-09-25 23:16:24 +0600

    Clock Fix
    Clock Speed Fix

--- Assets/Scripts/UI/ClockSpeedController.cs
+++ Assets/Scripts/UI/ClockSpeedController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClockSpeedController : MonoBehaviour
 {
     void Update()
     {
-        float v = gameObject.GetComponent<UnityEngine.UI.Slider>().value;
+        float v = gameObject.GetComponent<Slider>().value;
         GameObject.Find("Simulation").GetComponent<Simulation>().SetClockSpeed(v);
     }
 }
