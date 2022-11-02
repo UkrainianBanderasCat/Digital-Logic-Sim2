2ed49b79ab39227a42f8a89f2c5c7f45dd964c13
TheRiggedDev <109687890+TheRiggedDev@users.noreply.github.com>
2022-11-02 19:58:19 +0600

    Helpful and Fun Features
    All the Changes and Additions are listed down below -
    Changelog
    Splash Text
    3 More Toggle - Show FPS Toggle, Show Splash Text Toggle, Show Changelog Toggle
    Organized the Settings Menu
    Organized the Main Menu
    Improved Credit Menu UI.
    That's all the features or at least all that of them that I can remember.

---
+++ Assets/Scripts/UI/SplashTextManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SplashTextManager : MonoBehaviour
+{
+
+    [SerializeField] private List<string> splashLines;
+    [SerializeField] private TextMeshProUGUI splashTextUI;
+    [SerializeField] private bool showSplashText = true;
+    [SerializeField] private Toggle showSplashTextToggle;
+
+    void Start()
+    {
+        showSplashText = PlayerPrefs.GetInt("ShowSplashText") == 1 ? true : false;
+        showSplashTextToggle.isOn = showSplashText;
+        splashTextUI.text = splashLines[Random.Range(0, splashLines.Count)];
+        splashTextUI.gameObject.SetActive(showSplashText);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetShowSplashText(bool value)
+    {
+        showSplashText = value;
+        PlayerPrefs.SetInt("ShowSplashText", showSplashText ? 1 : 0);
+    }
+
+}
